5e4ee1f44790dde73042cf34a74db74e93a3de4c
dev-ahmedismael <131897456+dev-ahmedismael@users.noreply.github.com>
2023-08-21 02:18:10 +0300

    Version 1.1

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,27 @@
 import Header from "@/components/header/Header";
-import { Great_Vibes, Rajdhani } from "next/font/google";
+import { Rajdhani, Great_Vibes } from "next/font/google";
 import "/styles/globals.css";
 import Theme from "./theme";
 import Footer from "@/components/footer/Footer";
 
-export const rajdhani = Rajdhani({ subsets: ["latin"], weight: "400" });
-export const greatVibes = Great_Vibes({ subsets: ["latin"], weight: "400" });
-
-export const metadata = {
-  title: "DJoz",
-  description: "Liberty City DJ",
-};
+export const rajdhani = Rajdhani({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
+export const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>DJoz</title>
+        <meta name="description" content="Liberty City DJ" />
+      </head>
       <body className={rajdhani.className}>
         <Theme>
           <Header />
